9deb8a7d69e9510b0afa1d82e132788b49ff8b3d
Codindev24 <odinnmoddar@gmail.com>
2024-05-24 11:01:01 +0000

    Ideas workin on it

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { Login } from "./pages/Login";
+import { Home } from "./pages/Home";
+import { UserProvider, useUser } from "./lib/context/user";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import "./sass/nav.scss";
+
+function App() {
+  const isLoginPage = window.location.pathname === "/login";
+
+  return (
+    <div>
+      <UserProvider>
+        <Navbar /> {/* Add the navbar before page content */}
+        <main>{isLoginPage ? <Login /> : <Home />}</main>
+      </UserProvider>
+    </div>
+  );
+}
+
+function Navbar() {
+  const user = useUser();
+
+  return (
+
+    <nav className="flex justify-between">
+
+      <a href="/">Codindev / Quick blog</a>
+
+      <div className="user">
+
+        {user.current ? (
+
+          <>
+          
+            <span className="currentemail"><AccountCircleIcon/> {user.current.email}</span>
+
+            <button type="button" onClick={() => user.logout()}>
+              Logout
+            </button>
+
+          </>
+
+        ) : (
+
+          <a href="/login">Login</a>
+
+        )}
+
+      </div>{/* .user */}
+      
+    </nav>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { useUser } from "../lib/context/user";
+import { useIdeas } from "../lib/context/ideas";
+import "../sass/home.scss";
+
+export function Home() {
+  const user = useUser();
+  const ideas = useIdeas();
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div className="home">
+      {/* Show the submit form to logged in users. */}
+      {user.current ? (
+        <section className="flex justify-center">
+
+        <h2>Submit Idea</h2>
+
+          <form>
+
+           <div className="newpost">
+            <input
+              type="text"
+              placeholder="Title"
+              value={title}
+              onChange={(event) => {
+                setTitle(event.target.value);
+              }}
+            />
+            <textarea
+              placeholder="Description"
+              value={description}
+              onChange={(event) => {
+                setDescription(event.target.value);
+              }}
+            />
+            <button
+              type="button"
+              onClick={() =>
+                ideas.add({ userId: user.current.$id, title, description })
+              }
+            >
+              Submit
+            </button>
+           </div>{/* .newpost */}
+
+          </form>
+        </section>
+      ) : (
+        <section>
+          <p>Please login to submit an idea.</p>
+        </section>
+      )}
+      <section>
+        <h2>Latest Ideas</h2>
+        <ul>
+          {ideas && ideas.current.map((idea) => (
+            <li key={idea.$id}>
+              <strong>{idea.title}</strong>
+              <p>{idea.description}</p>
+              {/* Show the remove button to idea owner. */}
+              {user.current && user.current.$id === idea.userId && (
+                <button type="button" onClick={() => ideas.remove(idea.$id)}>
+                  Remove
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+}
